50f72a155567da7c68d8465a3eaa0e04dc35fc26
Tobias Abenius <tobbe@tobbe.nu>
2015-03-06 16:12:11 +0100

    possible to choose a smtp-server

--- lunchplans.py
+++ lunchplans.py
@@ -5,6 +5,7 @@
 # Purpose:
 #  Generate daily lunch menu summaries 
 
+
 from sys import argv, exit
 from email.mime.text import MIMEText
 import logging
@@ -14,6 +15,9 @@ import requests
 from restaurants import restaurants
 
 logger = logging.getLogger("lunchplans")
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+debugmode = True
 
 def get_lunch_menus():
     """ Get lunch menus from all restauraunts.
@@ -49,18 +53,35 @@ def parse_mailinglist(filename):
         logger.info("Found no email addresses in mailinglist.txt")
     return emails
 
+def get_smtp_server():
+    """ Get the SMTP server host, port
+    """
+    filename = "smtpserver.txt"
+    server = "localhost"
+    port = 25
+    try:
+	serverport = open(filename).readline().split(':')
+	server = serverport[0]
+	if (len(serverport) > 1):
+	    port = int(serverport[1])
+    except IOError:
+        logger.info("file '%s' not found, using default smtp server %s:%d" %(filename,server,port))
+    return dict(server=server, port=port)
 
 def send_emails(message, mailinglist, sender="lunchbot@mailinator.com"):
     """ Send emails with message to all recipients.
     """
     recipients = parse_mailinglist(mailinglist)
-    s = smtplib.SMTP("localhost")
-    msg = MIMEText(message, _charset="UTF-8")
-    msg["Subject"] = "Lunchplans?"
-    msg["From"] = sender
-    msg["To"] = ",".join(recipients)
-    s.sendmail(sender, "fredrik.boulund@chalmers.se", msg.as_string())
-
+    server = get_smtp_server()
+    if debugmode:
+        print message
+    else:
+	s = smtplib.SMTP(server['server'],port=server['port'])
+	msg = MIMEText(message, _charset="UTF-8")
+	msg["Subject"] = "Lunchplans?"
+	msg["From"] = sender
+	msg["To"] = ",".join(recipients)
+        s.sendmail(sender, "fredrik.boulund@chalmers.se", msg.as_string())
 
 if __name__ == "__main__":
     restaurant_menus = get_lunch_menus()
