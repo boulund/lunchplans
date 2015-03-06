25dc00bf122ebdbba2c1a6a432a8e86e38d39b88
Tobias Abenius <tobbe@tobbe.nu>
2015-03-06 16:11:36 +0100

    Changed from BS to proper XML parsing with ElementTree

--- restaurants/timeout.py
+++ restaurants/timeout.py
@@ -2,11 +2,14 @@
 # Module: Time Out Bistro
 # Authors:
 #  Fredrik Boulund
+#  Tobias Abenius
 
 from datetime import datetime
-from bs4 import BeautifulSoup as bs
+#from bs4 import BeautifulSoup as bs
+import xml.etree.cElementTree as xet
 import logging
 import requests
+import pdb
 
 name = "Time Out Bistro"
 
@@ -21,17 +24,29 @@ def todays_lunch():
     logging.debug("Downloading the RSS for Time Out Bistro from their facebook page on {}".format(today))
     rss = "https://www.facebook.com/feeds/page.php?format=rss20&id=135890329899423"
     rss_xml = requests.get(rss)
-    soup = bs(rss_xml.text)
+#    soup = bs(rss_xml.text)
+    tree = xet.fromstring(rss_xml.text.encode('utf-8'))
+    chan = tree.getchildren()[0]
     
     menu_items = []
-    for item in soup.find_all("item"):
-        desc = item.description.string.split("<br")
-        day = desc[0]
-        if weekday in day.split()[0]:
-            regular = desc[3].strip(" <>/")
-            veg = desc[6].strip(" <>/")
+    for item in chan.findall("item"):
+#	pdb.set_trace()
+        desc = item.find('description').text
+        #desc = u"".join(unicode(a) for a in item.description.contents).split(u"<br")
+        #desc = [unicode(a) for a in desc]
+        #desc = [a.strip(u" <>/") for a in desc]
+	desc = [a.replace("/>","").replace(">","").strip() for a in desc.split("<br") ]
+	desc = filter(lambda e: e!="" and e!="br", desc)
+	day = desc[0].split()[0]
+        #desc = item.description.split("<br")
+        #day = desc[0]
+	if weekday in day:
+	    if desc[1].lower().startswith("dagens"):
+              meat = desc[2].strip(" <>/")
+       	    if desc[3].lower().startswith("veg"):
+              veg = desc[4].strip(" <>/")
             logging.debug("Found menu for {}".format(day))
-            menu_items.append(regular)
+            menu_items.append(meat)
             menu_items.append(veg)
             break
     else:
